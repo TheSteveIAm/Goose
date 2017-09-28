918a313fe291084a4d58ed7f33a9e3c239eedb50
SChubatyA <SChubatyA@nunavut.local>
2017-09-28 16:54:28 -0500

    My most important work to date

--- Assets/Scripts/GooseController.cs
+++ Assets/Scripts/GooseController.cs
@@ -24,28 +24,29 @@ public class GooseController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float motion = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
-        float rotation = Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime;
+        //float motion = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        float motion = 1 * moveSpeed * Time.deltaTime;
+        //float rotation = Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime;
 
-        float speed = 0;
+        //float speed = 0;
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            speed = 0.5f;
-            motion /= 2;
-        }
+        //if (Input.GetKey(KeyCode.W))
+        //{
+        //    speed = 0.5f;
+        //    motion /= 2;
+        //}
 
-        if(Input.GetKey(KeyCode.LeftShift))
-        {
-            speed = 1f;
-            motion *= 2;
-        }
+        //if(Input.GetKey(KeyCode.LeftShift))
+        //{
+        //    speed = 1f;
+        //    motion *= 2;
+        //}
 
-        anim.SetFloat("LegSpeed", speed);
+        //anim.SetFloat("LegSpeed", speed);
 
         body.MovePosition(transform.position + transform.forward * motion);
 
-        body.MoveRotation(transform.rotation * Quaternion.Euler(0, rotation, 0));
+        //body.MoveRotation(transform.rotation * Quaternion.Euler(0, rotation, 0));
 
     }
 }
---
+++ Assets/Scripts/Tele.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tele : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider col)
+    {
+        //Debug.Log(col.transform.name);
+        col.transform.Translate(Vector3.back * 47);
+    }
+}
